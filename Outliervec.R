5ee370716d94f6628b867f6b1bf3b7383d37557e
vmellbin <vmellb20@student.aau.dk>
2024-05-27 13:51:46 +0200

    Initial commit

---
+++ Outliervec.R
@@ -0,0 +1,25 @@
+#' Detects observations that are more than a given number of standard devations away from the mean
+#'
+#' @param vector The vector where we detect outliers.
+#' @param std The standard deviation for outliers to be detected
+#'
+#' @return Returns S3 object containing the index of the outliers
+#' @export
+#'
+#' @examples vector <- 1:10
+#' OutlierVec(vector)
+#' 
+#'
+OutlierVec<- function(vector, std){
+  Mean <-mean(vector)
+  Stddeviation <- sd(vector)
+  lvector<-1:length(vector)
+  vector1 <-(vector > Mean-Antalstd*Stddeviation & vector < Mean+Antalstd*Stddeviation)
+  Outliervecdetect<-which(vector1 %in% 0)
+  class(z)<- c("Outliervec")
+  z$mean<- Mean
+  z$std<- Stddeviation
+  z$outliers<- Outliervecdetect
+  z
+}
+
